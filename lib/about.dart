a32fa33583cae77ace130029404258ebb9731e3f
ElijahOd <124442472+ElijahOd@users.noreply.github.com>
2023-05-19 14:12:56 +0300

    Update about.dart

--- lib/about.dart
+++ lib/about.dart
@@ -37,34 +37,63 @@ class _AboutPageState extends State<AboutPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Flutter app's by OD")),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Column(
-                children: [
-                  Image.network(
-                    widget.cardInfo.imageUrl,
-                    width: double.infinity,
-                    //height: 230,
-                    fit: BoxFit.cover,
-                  ),
-                  const SizedBox(height: 32),
-                  TextField(controller: controller),
-                ],
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(
+    return WillPopScope(
+      onWillPop: () async {
+        bool confirmExit = await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Warning'),
+            content:
+                const Text('Are you sure you want to leave without saving?'),
+            actions: [
+              TextButton(
                 onPressed: () {
-                  save();
-
-                  Navigator.of(context).pop(cardInfoNew);
+                  Navigator.of(context).pop(true);
                 },
-                child: const Text('Save'),
+                child: const Text(
+                  'Yes',
+                  style: TextStyle(color: Color(0xffff0000)),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('No'),
               ),
             ],
+          ),
+        );
+        return confirmExit == true;
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Flutter app's by OD")),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Image.network(
+                      widget.cardInfo.imageUrl,
+                      width: double.infinity,
+                      //height: 230,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(height: 32),
+                    TextField(controller: controller),
+                  ],
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {
+                    save();
+                    Navigator.of(context).pop(cardInfoNew);
+                  },
+                  child: const Text('Save'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
